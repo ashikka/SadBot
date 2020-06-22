db7455d5804d6de9dc13c3b50f63bad442d084c4
ashikka <ashikagupta28@gmail.com>
2020-06-22 20:01:22 +0530

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express');
+var app = express()
+var bodyParser = require('body-parser')
+const axios = require('axios')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.post('/new-message', function (req, res) {
+    const {message} = req.body
+
+
+    if (!message || message.text.toLowerCase().indexOf('marco') < 0) {
+
+        return res.end()
+    }
+
+
+    axios.post('https://api.telegram.org/'+process.env.TOKEN+'/sendMessage', {
+        chat_id: message.chat.id,
+        text: 'Polo!!'
+    }).then(response => {
+
+        console.log('Message posted')
+        res.end('ok')
+    }).catch(err => {
+
+        console.log('Error :', err)
+        res.end('Error :' + err)
+    })
+})
+
+
+app.listen(3000, function () {
+    console.log('Telegram app listening on port 3000!')
+});
